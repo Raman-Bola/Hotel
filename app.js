b9277a5b849c4d5b8c4202f4bdb48f79ea8b45b9
Raman-Bola <raman22062002@gmail.com>
2022-07-04 16:59:07 +0530

    helmet

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const passport=require('passport');
 const LocalStrategy= require('passport-local');
 const User=require('./models/user');
 const mongoSanitize= require('express-mongo-sanitize');
-const helmet= require('helmet');
 
 const mongoStore= require("connect-mongo")(session);
 
@@ -77,59 +76,6 @@ const sessionConfig={
 }
 app.use(session(sessionConfig));
 app.use(flash());
-app.use(helmet({ crossOriginEmbedderPolicy: false,contentSecurityPolicy:false}));
-
-const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net/",
-    "https://res.cloudinary.com/dv5vm4sqh/"
-];
-const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net/",
-    "https://res.cloudinary.com/dv5vm4sqh/"
-];
-const connectSrcUrls = [
-    "https://*.tiles.mapbox.com",
-    "https://api.mapbox.com",
-    "https://events.mapbox.com",
-    "https://res.cloudinary.com/dv5vm4sqh/"
-];
-const fontSrcUrls = [ "https://res.cloudinary.com/dv5vm4sqh/" ];
- 
-app.use(
-    helmet.contentSecurityPolicy({
-        directives : {
-            defaultSrc : [],
-            connectSrc : [ "'self'", ...connectSrcUrls ],
-            scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
-            styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
-            workerSrc  : [ "'self'", "blob:" ],
-            objectSrc  : [],
-            imgSrc     : [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/", 
-                "https://images.unsplash.com/"
-            ],
-            fontSrc    : [ "'self'", ...fontSrcUrls ],
-            childSrc   : [ "blob:" ]
-        },
-        crossOriginEmbedderPolicy: false    
-    })
-);
-
-
 
 
 app.use(passport.initialize());
